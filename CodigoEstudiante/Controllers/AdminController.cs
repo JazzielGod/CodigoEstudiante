28ec58670ab6af3a7d600a3117963b5de94a2755
JazzielGod <jazzielbh@gmail.com>
2026-01-14 17:22:10 -0700

    Creación del controlador administrativo y ViewModel para estadísticas. Vista del Dashboard con métricas de productos y categorías.

---
+++ CodigoEstudiante/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using CodigoEstudiante.Models;
+using CodigoEstudiante.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CodigoEstudiante.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController(
+        CategoryService _categoryService,
+        ProductService _productService
+    ) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var products = await _productService.GetAllAsync();
+
+            var viewModel = new DashboardVM
+            {
+                CategoryCount = categories.Count(),
+                ProductCount = products.Count()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CodigoEstudiante/Models/DashboardVM.cs
@@ -0,0 +1,8 @@
+﻿namespace CodigoEstudiante.Models
+{
+    public class DashboardVM
+    {
+        public int CategoryCount { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
